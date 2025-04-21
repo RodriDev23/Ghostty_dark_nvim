c767eb45cc4563e06b8608e72cdc5c22c10506ee
Rodri <RodriDev23@outlook.com>
2025-04-21 16:00:34 -0400

    testing colors for cmp

--- lua/ghostty_dark/integrations/cmp.lua
+++ lua/ghostty_dark/integrations/cmp.lua
@@ -3,45 +3,63 @@ local colorscheme = require 'ghostty_dark.colorscheme'
 local M = {}
 
 function M.highlights()
-  return {
-
-    -- testing if this is the item selected in cmp
-    CmpItemAbbr = { fg = colorscheme.successText },
-    CmpItemAbbrDeprecated = { fg = colorscheme.mainText, strikethrough = true },
-    CmpItemKind = { fg = colorscheme.syntaxFunction },
-    CmpItemMenu = { fg = colorscheme.mainText },
-    CmpItemAbbrMatch = { fg = colorscheme.successText, bold = true },
-    CmpItemAbbrMatchFuzzy = { fg = colorscheme.syntaxFunction, bold = true },
+    return {
 
 
-    -- kind support
-    CmpItemKindSnippet = { fg = colorscheme.stringText },
-    CmpItemKindKeyword = { fg = colorscheme.syntaxError },
-    CmpItemKindText = { fg = colorscheme.warningEmphasis },
-    CmpItemKindMethod = { fg = colorscheme.syntaxKeyword },
-    CmpItemKindConstructor = { fg = colorscheme.syntaxKeyword },
-    CmpItemKindFunction = { fg = colorscheme.syntaxKeyword },
-    CmpItemKindFolder = { fg = colorscheme.syntaxKeyword },
-    CmpItemKindModule = { fg = colorscheme.syntaxKeyword },
-    CmpItemKindConstant = { fg = colorscheme.syntaxKeyword },
-    CmpItemKindField = { fg = colorscheme.syntaxKeyword },
-    CmpItemKindProperty = { fg = colorscheme.successText },
-    CmpItemKindEnum = { fg = colorscheme.successText },
-    CmpItemKindUnit = { fg = colorscheme.successText },
-    CmpItemKindClass = { fg = colorscheme.warningText },
-    CmpItemKindVariable = { fg = colorscheme.syntaxError },
-    CmpItemKindFile = { fg = colorscheme.linkText },
-    CmpItemKindInterface = { fg = colorscheme.stringText },
-    CmpItemKindColor = { fg = colorscheme.syntaxError },
-    CmpItemKindReference = { fg = colorscheme.syntaxError },
-    CmpItemKindEnumMember = { fg = colorscheme.syntaxError },
-    CmpItemKindStruct = { fg = colorscheme.syntaxFunction },
-    CmpItemKindValue = { fg = colorscheme.warningEmphasis },
-    CmpItemKindEvent = { fg = colorscheme.syntaxFunction },
-    CmpItemKindOperator = { fg = colorscheme.syntaxError },
-    CmpItemKindTypeParameter = { fg = colorscheme.syntaxError },
-    CmpItemKindCopilot = { fg = colorscheme.linkText },
-  }
+        -- this is not
+        CmpItemAbbr = { fg = colorscheme.mainText },
+        -------------------------------------------------
+
+        -- this is not ----------------------------
+        CmpItemAbbrDeprecated = { fg = colorscheme.mainText, strikethrough = true },
+        -------------------------------------------------------------------------
+
+
+
+
+        CmpItemKind = { fg = colorscheme.syntaxFunction },
+
+
+
+
+        -- testing this is the color for selected item
+        CmpItemMenu = { fg = colorscheme.successText },
+        ----------------------------------------------
+
+
+
+        CmpItemAbbrMatch = { fg = colorscheme.successText, bold = true },
+        CmpItemAbbrMatchFuzzy = { fg = colorscheme.syntaxFunction, bold = true },
+
+
+        -- kind support
+        CmpItemKindSnippet = { fg = colorscheme.stringText },
+        CmpItemKindKeyword = { fg = colorscheme.syntaxError },
+        CmpItemKindText = { fg = colorscheme.warningEmphasis },
+        CmpItemKindMethod = { fg = colorscheme.syntaxKeyword },
+        CmpItemKindConstructor = { fg = colorscheme.syntaxKeyword },
+        CmpItemKindFunction = { fg = colorscheme.syntaxKeyword },
+        CmpItemKindFolder = { fg = colorscheme.syntaxKeyword },
+        CmpItemKindModule = { fg = colorscheme.syntaxKeyword },
+        CmpItemKindConstant = { fg = colorscheme.syntaxKeyword },
+        CmpItemKindField = { fg = colorscheme.syntaxKeyword },
+        CmpItemKindProperty = { fg = colorscheme.successText },
+        CmpItemKindEnum = { fg = colorscheme.successText },
+        CmpItemKindUnit = { fg = colorscheme.successText },
+        CmpItemKindClass = { fg = colorscheme.warningText },
+        CmpItemKindVariable = { fg = colorscheme.syntaxError },
+        CmpItemKindFile = { fg = colorscheme.linkText },
+        CmpItemKindInterface = { fg = colorscheme.stringText },
+        CmpItemKindColor = { fg = colorscheme.syntaxError },
+        CmpItemKindReference = { fg = colorscheme.syntaxError },
+        CmpItemKindEnumMember = { fg = colorscheme.syntaxError },
+        CmpItemKindStruct = { fg = colorscheme.syntaxFunction },
+        CmpItemKindValue = { fg = colorscheme.warningEmphasis },
+        CmpItemKindEvent = { fg = colorscheme.syntaxFunction },
+        CmpItemKindOperator = { fg = colorscheme.syntaxError },
+        CmpItemKindTypeParameter = { fg = colorscheme.syntaxError },
+        CmpItemKindCopilot = { fg = colorscheme.linkText },
+    }
 end
 
 return M
