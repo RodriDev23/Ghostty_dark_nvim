3a095c8be7b31c706a448a993061d80787444688
Rodri <RodriDev23@outlook.com>
2025-04-22 21:36:46 -0400

    fixing colors from bufferline plugin

--- lua/ghostty_dark/integrations/bufferline.lua
+++ lua/ghostty_dark/integrations/bufferline.lua
@@ -6,10 +6,14 @@ function M.highlights(config)
     local bg = config.transparent and 'NONE' or colorscheme.sidebarBackground
     local white_color = "#FFFFFF"
     return {
-        buffer_visible = { fg = white_color, bg = bg },
+        buffer_visible = {
+            fg = white_color,
+            bg = bg,
+        },
         buffer_selected = {
             fg = white_color,
             bg = colorscheme.editorBackground,
+            bold = true
         },
         tab_selected = {
             fg = white_color,
