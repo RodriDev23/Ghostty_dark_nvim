c8d2c3a4b917e8fcd5390f723a8f456d1d291f25
Rodri <RodriDev23@outlook.com>
2025-04-22 21:52:15 -0400

    fixing colors from bufferlinelualine plugin

--- lua/lualine/themes/ghostty_dark.lua
+++ lua/lualine/themes/ghostty_dark.lua
@@ -3,62 +3,63 @@ local config = require 'ghostty_dark.config'
 local theme = {}
 
 local bg = config.transparent and 'NONE' or colorscheme.editorBackground
+local white_color = "#FFFFFF"
 
 theme.normal = {
-  a = { bg = colorscheme.blue, fg = colorscheme.editorBackground, gui = 'bold' },
-  b = { bg = bg, fg = colorscheme.commentText },
-  c = { bg = bg, fg = colorscheme.editorBackground },
+    a = { bg = colorscheme.blue, fg = white_color, gui = 'bold' },
+    b = { bg = bg, fg = white_color },
+    c = { bg = bg, fg = white_color },
 }
 
 theme.insert = {
-  a = {
-    bg = colorscheme.syntaxError,
-    fg = colorscheme.editorBackground,
-    gui = 'bold',
-  },
-  b = { bg = bg, fg = colorscheme.commentText },
+    a = {
+        bg = "#FFFFFF",
+        fg = "#0000FF",
+        gui = 'bold',
+    },
+    b = { bg = bg, fg = colorscheme.commentText },
 }
 
 theme.terminal = {
-  a = {
-    bg = colorscheme.successText,
-    fg = colorscheme.editorBackground,
-    gui = 'bold',
-  },
-  b = { bg = bg, fg = colorscheme.commentText },
+    a = {
+        bg = colorscheme.successText,
+        fg = white_color,
+        gui = 'bold',
+    },
+    b = { bg = bg, fg = colorscheme.commentText },
 }
 
 theme.command = {
-  a = {
-    bg = colorscheme.successText,
-    fg = colorscheme.editorBackground,
-    gui = 'bold',
-  },
-  b = { bg = bg, fg = colorscheme.commentText },
+    a = {
+        bg = colorscheme.successText,
+        fg = white_color,
+        gui = 'bold',
+    },
+    b = { bg = bg, fg = colorscheme.commentText },
 }
 
 theme.visual = {
-  a = {
-    bg = colorscheme.syntaxKeyword,
-    fg = colorscheme.editorBackground,
-    gui = 'bold',
-  },
-  b = { bg = bg, fg = colorscheme.commentText },
+    a = {
+        bg = colorscheme.syntaxKeyword,
+        fg = white_color,
+        gui = 'bold',
+    },
+    b = { bg = bg, fg = colorscheme.commentText },
 }
 
 theme.replace = {
-  a = {
-    bg = colorscheme.warningText,
-    fg = colorscheme.editorBackground,
-    gui = 'bold',
-  },
-  b = { bg = bg, fg = colorscheme.commentText },
+    a = {
+        bg = colorscheme.warningText,
+        fg = white_color,
+        gui = 'bold',
+    },
+    b = { bg = bg, fg = colorscheme.commentText },
 }
 
 theme.inactive = {
-  a = { bg = colorscheme.commentText, fg = colorscheme.editorBackground },
-  b = { bg = bg, fg = colorscheme.warningText, gui = 'bold' },
-  c = { bg = bg, fg = colorscheme.warningText },
+    a = { bg = colorscheme.commentText, fg = white_color },
+    b = { bg = bg, fg = colorscheme.warningText, gui = 'bold' },
+    c = { bg = bg, fg = colorscheme.warningText },
 }
 
 return theme
